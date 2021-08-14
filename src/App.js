89c4b6ed96a545e03112c293f7c9e1ae424de7bb
brijhaywood <briyanahaywood@gmail.com>
2021-08-13 23:24:51 -0500

    Log-In, Log-out, Register

--- src/App.js
+++ src/App.js
@@ -1,20 +1,3 @@
-// import React, { useEffect, useState } from 'react';
-
-// import './App.css';
-// import HeroSection from './components/HeroSection'
-// // import Posts from './components/Map';
-// // import PostLoadingComponent from './components/MapLoading';
-
-// function App() {
-//   return(
-//     <div>
-//       <HeroSection></HeroSection>
-//     </div>
-//   )
-// }
-// export default App;
-
-  
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import Map from './components/Map';
